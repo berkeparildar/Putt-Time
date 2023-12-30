5ec0f0fe17b7dea2e39f849058c9ecc71dc2922c
Berke Parıldar <94047726+berkeparildar@users.noreply.github.com>
2023-12-30 22:35:05 +0300

    Setting up the game logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,27 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using ExitGames.Client.Photon;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public enum EventCode
 {
-    void Start() { }
+    INHOLE = 0,
+    NEXTHOLE = 1
+}
+
+public class GameManager : MonoBehaviour, IOnEventCallback
+{
+    private int _currentHole;
+    private const int MaxNumberOfHoles = 6;
+    public int playersInHole;
+    private List<GameObject> players;
+
+    private void Start()
+    {
+        _currentHole = 0;
+    }
 
     private void OnEnable()
     {
-        Player.InHole += GoToNextHole;
+        PhotonNetwork.AddCallbackTarget(this);
     }
 
     private void OnDisable()
     {
-        Player.InHole -= GoToNextHole;
+        PhotonNetwork.RemoveCallbackTarget(this);
     }
 
-    void Update()
+    public void OnEvent(EventData photonEvent)
     {
-        Debug.Log(PhotonNetwork.CurrentRoom.PlayerCount);
+        if (PhotonNetwork.IsMasterClient && photonEvent.Code == 0)
+        {
+           playersInHole++;
+        }
     }
 
-    private void GoToNextHole()
+    private void Update()
     {
-        Debug.Log("Event");
+        CheckIfNextHole();
+    }
+
+    public static byte GetEventCode(EventCode eventcode)
+    {
+        byte code = 0;
+        switch (eventcode)
+        {
+            case EventCode.INHOLE:
+                code = 0;
+                break;
+            case EventCode.NEXTHOLE:
+                code = 1;
+                break;
+        }
+        return code;
+    }
+
+    public static void RaiseEventToAll(byte code, object data)
+    {
+        Debug.Log("Sending event");
+        var eventOpitons = RaiseEventOptions.Default;
+        eventOpitons.Receivers = ReceiverGroup.All;
+        PhotonNetwork.RaiseEvent(code, data, eventOpitons, SendOptions.SendReliable);
+    }
+
+    private IEnumerator GoToNextHoleEvent()
+    {
+        yield return new WaitForSeconds(3);
+        RaiseEventToAll(GetEventCode(EventCode.NEXTHOLE), new Vector3(0, 0, 0));
+    }
+
+    private void CheckIfNextHole()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            if (playersInHole == PhotonNetwork.CurrentRoom.PlayerCount)
+            {
+                playersInHole = 0;
+                StartCoroutine(GoToNextHoleEvent());
+            }
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,31 @@
 using System;
 using Cinemachine;
+using ExitGames.Client.Photon;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, IOnEventCallback
 {
-    public static Action InHole;
-
-    [SerializeField]
-    private Vector3 spawnPosition;
-
+    [SerializeField] private Vector3 spawnPosition;
     private CinemachineVirtualCamera _virtualCamera;
-
     private PhotonView _photonView;
+    private bool _inHole;
 
     void Start()
     {
         _photonView = GetComponent<PhotonView>();
         SetUpCamera();
+    }
+
+     private void OnEnable()
+    {
+        PhotonNetwork.AddCallbackTarget(this);
+    }
+
+    private void OnDisable()
+    {
+        PhotonNetwork.RemoveCallbackTarget(this);
     }
 
     private void Update() { }
@@ -36,9 +44,21 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Hole"))
+        if (other.CompareTag("Hole") && !_inHole)
         {
-            InHole?.Invoke();
+            _inHole = true;
+            GameManager.RaiseEventToAll(GameManager.GetEventCode(EventCode.INHOLE), null);
+        }
+    }
+
+    public void OnEvent(EventData photonEvent)
+    {
+        if (_photonView.IsMine)
+        {
+            if (photonEvent.Code == GameManager.GetEventCode(EventCode.NEXTHOLE))
+            {
+                transform.position = (Vector3)photonEvent.CustomData;
+            }
         }
     }
 }
